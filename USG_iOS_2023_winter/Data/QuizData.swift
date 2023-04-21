fc63624e5e9124ccb0cb439035a3e4c56cae1642
munchangjae <107340769+munchangjae@users.noreply.github.com>
2023-04-21 20:22:17 +0900

    QuizData, QuizModel, QuizViewModel

---
+++ USG_iOS_2023_winter/Data/QuizData.swift
@@ -0,0 +1,15 @@
+//
+//  QuizData.swift
+//  USG_iOS_2023_winter
+//
+//  Created by 문창재 on 2023/04/21.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct QuizData {
+    
+    static let rabbitQuiz = [ QuizModel(number: <#T##String#>, subTopic: <#T##Image#>, question: <#T##[Question]#>)]
+}
---
+++ USG_iOS_2023_winter/ViewModels/QuizViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  QuizViewModel.swift
+//  USG_iOS_2023_winter
+//
+//  Created by 문창재 on 2023/04/21.
+//
+
+import Foundation
+
+import SwiftUI
+
+class QuizViewModel: ObservableObject {
+    
+    func selectAnswer() {
+        var number: Int
+        
+    }
+}
